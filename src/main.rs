041197c48fa84e280ae2ff99b4f26a4eccba8c98
hzhong <heatherzhong@gmail.com>
2023-12-01 23:05:18 -0500

    day1 axum extractor exercise

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use axum::{extract::Path, routing::get, Router};
+use std::ops::BitXor;
+
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+async fn calculate(params: Path<(u64, u64)>) -> String {
+    let nums = params.0;
+    let (num1, num2) = nums;
+
+    let result = (num1 ^ num2).pow(3);
+
+    format!("{}", result)
+}
+
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
+    let router = Router::new()
+        .route("/1/:num1/:num2", get(calculate))
+        .route("/", get(hello_world));
+
+    Ok(router.into())
+}
